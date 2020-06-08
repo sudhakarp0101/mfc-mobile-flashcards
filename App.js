f13c77e1ab8533e94b85fba45ceecfde1c229046
sudhakarp0101 <sudhakarp0101@gmail.com>
2020-06-08 16:11:11 +0530

    commit

--- App.js
+++ App.js
@@ -1,36 +1,66 @@
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index';
+import MfcDeckList from './components/MfcDeckList'
+import MfcDeckDetail from './components/MfcDeckDetail'
+import MfcAddDeck from './components/MfcAddDeck';
+import MfcAddCard from './components/MfcAddCard';
+import MfcQuiz from './components/MfcQuiz'
+import { setLocalNotification } from './utils/helpers';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
 
-export default function App() {
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger)
+);
+
+const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="MfcDeckList" component={MfcDeckList} options={{ headerShown: false }} />
+      <Stack.Screen name="MfcDeckDetail" component={MfcDeckDetail} />
+      <Stack.Screen name="MfcAddCard" component={MfcAddCard} />
+      <Stack.Screen name="MfcQuiz" component={MfcQuiz} />
+    </Stack.Navigator>
+
   );
 }
-
+export default class App extends React.Component {
+  componentDidMount() {
+    setLocalNotification();
+  }
+  render() {
+    return (
+      <Provider store={store}>
+        <View style={styles.container}>
+          <NavigationContainer>
+            <Tab.Navigator tabBarOptions={{ labelStryle: { fontSize: 20 } }}>
+              <Tab.Screen name="DECKS" component={Home} />
+              <Tab.Screen name="New Deck" component={MfcAddDeck} />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </View>
+      </Provider>
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#dde'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  tabfs: {
+    fontSize: 30
+  }
 });
